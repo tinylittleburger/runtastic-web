d8cd22f7de207de48665428c528532c90bb4767f
tinylittleburger <tinylittleburger@gmail.com>
2017-04-09 13:24:32 +0200

    Implemented export to zip function

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"net/http"
+	"time"
 
+	"io"
+
+	"github.com/metalnem/runtastic/api"
+	"github.com/metalnem/runtastic/tcx"
+)
+
+func formatTime(t time.Time) string {
+	return t.Local().Format("2006-01-02 15.04.05")
+}
+
+func export(activities []api.Activity, w io.Writer) error {
+	zw := zip.NewWriter(w)
+	defer zw.Close()
+
+	for _, activity := range activities {
+		t := formatTime(activity.EndTime)
+		filename := fmt.Sprintf("Runtastic %s %s.tcx", t, activity.Type.DisplayName)
+
+		header := zip.FileHeader{
+			Name:   filename,
+			Method: zip.Deflate,
+		}
+
+		header.SetModTime(time.Now())
+		file, err := zw.CreateHeader(&header)
+
+		if err != nil {
+			return err
+		}
+
+		exp := tcx.NewExporter(file)
+
+		if err = exp.Export(activity); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		api.Login(context.Background(), "", "")
+	})
+
+	http.ListenAndServe(":80", nil)
 }
